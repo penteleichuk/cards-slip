a028600d25a34118d8318735d425aa79ede275e2
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-29 13:54:17 +0300

    login page

--- src/pages/auth/login/s3-dal/LoginApi.ts
+++ src/pages/auth/login/s3-dal/LoginApi.ts
@@ -3,7 +3,7 @@ import {instance} from "../../../app/s3-dal/instance";
 
 export const loginApi = {
     login(data:LoginParamsType) {
-        return instance.post<ResponseType<LoginParamsType>>('auth/login', data)
+        return instance.post<LoginParamsType>('auth/login', data)
     }
 
 };
@@ -12,24 +12,5 @@ export type LoginParamsType = {
     password: string,
     rememberMe?: boolean,
 }
-export type ResponseType<D = {}> = {
-    resultCode: number
-    messages: Array<string>
-    fieldsErrors: Array<string>
-    data: D
-}
-type LoginType = {
-    _id: string;
-    email: string;
-    name: string;
-    avatar?: string;
-    publicCardPacksCount: number;
-// количество колод
-    created: Date;
-    updated: Date;
-    isAdmin: boolean;
-    verified: boolean; // подтвердил ли почту
-    rememberMe: boolean;
 
-    error?: string;
-}
+
