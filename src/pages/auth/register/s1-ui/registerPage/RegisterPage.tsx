abc4846454f676c0e83fc87fb1eeb317d99125eb
VictoriaKirplyuk <meosh99@bk.ru>
2022-05-29 06:09:19 +0300

    Added container for register component. Component names fixed

--- src/pages/auth/register/s1-ui/registerPage/RegisterPage.tsx
+++ src/pages/auth/register/s1-ui/registerPage/RegisterPage.tsx
@@ -1,44 +1,30 @@
-import {useState} from "react";
-import RegistrationForm from "../registerForm/RegistrationForm";
-import {useDispatch, useSelector} from "react-redux";
-import {UserDataType} from "../../s3-dal/RegisterApi";
-import {setRegisterUserTC} from "../../s2-bll/thunks/RegisterThunks";
-import {AppStoreType} from "../../../../app/s2-bll/store";
-import { Navigate } from "react-router-dom";
 import s from "./RegisterPage.module.css"
-import {setErrorMessageAC} from "../../s2-bll/RegisterActions";
 import ErrorWindow from "../errorWindow/ErrorWindow";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../../../../app/s2-bll/store";
+import {setErrorMessageAC} from "../../s2-bll/RegisterActions";
+import RegisterContainer from "../registerContainer/RegisterContainer";
+import {FC, useCallback} from "react";
+import {Navigate} from "react-router-dom";
 
-export const RegisterPage = (): JSX.Element => {
+export const RegisterPage: FC = (): JSX.Element => {
 
-    ////display on RegisterPageContainer? to fix
-    const [email, setEmail] = useState<string>('')
-    const [pass, setPass] = useState<string>('')
-    const [confirmPass, setConfirmPass] = useState<string>('')
-
-    const errorMessage = useSelector<AppStoreType, string | null>(state => state.register.errorMessage)
     const isRegistered = useSelector<AppStoreType, boolean>(state => state.register.isRegistered)
-    const dispatch = useDispatch<any>() //to fix
+    const errorMessage = useSelector<AppStoreType, string | null>(state => state.register.errorMessage)
+    const dispatch = useDispatch<any>()
 
-    const setRegister = (userData: UserDataType) => {
-        dispatch(setRegisterUserTC(userData))
-    }
-    const setErrorMessage = (errorMessage: string | null) => {
+    const setErrorMessage = useCallback((errorMessage: string | null) => {
         dispatch(setErrorMessageAC(errorMessage))
-    } //display on windowErrorContainer? to fix
+    }, [dispatch])
 
-    if(isRegistered) {
+    if (isRegistered) {
         return <Navigate to={"/login"}/>
     }
 
     return (
         <div className={s.wrapper}>
             <div>
-                <RegistrationForm email={email} setEmail={setEmail}
-                                  pass={pass} setPass={setPass}
-                                  confirmPass={confirmPass} setConfirmPass={setConfirmPass}
-                                  setRegister={setRegister}
-                />
+                <RegisterContainer />
             </div>
             {errorMessage && <ErrorWindow error={errorMessage} setError={setErrorMessage}/>}
         </div>
