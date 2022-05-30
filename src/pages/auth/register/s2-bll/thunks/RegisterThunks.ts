884338cfb89210ab9eae0b3c34aa2699b8cca081
VictoriaKirplyuk <meosh99@bk.ru>
2022-05-30 21:10:13 +0300

    Thunk fixed(async/await), added error type in catch

--- src/pages/auth/register/s2-bll/thunks/RegisterThunks.ts
+++ src/pages/auth/register/s2-bll/thunks/RegisterThunks.ts
@@ -2,18 +2,24 @@ import {RegisterActionsType, setErrorMessageAC, setRegisterAC, setRequestStatusA
 import {Dispatch} from "redux";
 import {registerApi, UserDataType} from "../../s3-dal/RegisterApi";
 import {RequestStatus} from "../RegisterInitState";
+import axios from "axios";
 
+type TodoErrorResponse = {
+    error: string
+}
 
-export const setRegisterUserTC = (userData: UserDataType) => (dispatch: Dispatch<RegisterActionsType>) => {
+export const setRegisterUserTC = (userData: UserDataType) => async (dispatch: Dispatch<RegisterActionsType>) => {
     dispatch(setRequestStatusAC(RequestStatus.inProgress))
-    registerApi.register(userData)
-        .then(() => {
-                dispatch(setRegisterAC(true))
-                dispatch(setRequestStatusAC(RequestStatus.succeeded))
-                dispatch(setErrorMessageAC(null))
-        })
-        .catch(e => {
-            dispatch(setErrorMessageAC(e.response.data.error))
-            dispatch(setRequestStatusAC(RequestStatus.failed))
-        })
+    try {
+        await registerApi.register(userData)
+        dispatch(setRegisterAC(true))
+        dispatch(setRequestStatusAC(RequestStatus.succeeded))
+        dispatch(setErrorMessageAC(null))
+    } catch (err) {
+        if(axios.isAxiosError(err) && err.response) {
+            console.log(err.response?.data)
+            dispatch(setErrorMessageAC((err.response?.data as TodoErrorResponse).error))
+        }
+        dispatch(setRequestStatusAC(RequestStatus.failed))
+    }
 }
