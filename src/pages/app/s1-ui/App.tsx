c487fbeaaaf4ef1fea3e9b3105dd7e8549fce9dc
vasyok28 <waitoatime@gmail.com>
2022-06-03 16:17:11 +0300

    fix css dialog form width

--- src/pages/app/s1-ui/App.tsx
+++ src/pages/app/s1-ui/App.tsx
@@ -1,8 +1,8 @@
 import {Route, Routes} from "react-router-dom";
 import {Header} from "../../../components/_Pages/Header/Header";
 import {routes} from "../../../routes";
-import './App.scss';
 import {Alert} from "../../../components/Alert/Alert";
+import './App.scss';
 
 export const App = () => {
     return (
