0ce2c6d457ba06a8c0fe5bff4b5d6b1cba5c0132
vasyok28 <waitoatime@gmail.com>
2022-06-10 13:05:33 +0300

    show pagination > 1

--- src/components/Paginated/PaginatedPage.tsx
+++ src/components/Paginated/PaginatedPage.tsx
@@ -10,17 +10,17 @@ type PaginatedPageType = {
 export const PaginatedPage = ({totalCards, countPages, onPageChanged, currentPage}: PaginatedPageType) => {
     const totalPages = Math.ceil(totalCards / countPages)
 
-    let pages = [];
+    let pages: number[] = [];
     if (totalPages > 10) {
         if (currentPage > 5) {
             for (let i = currentPage - 4; i <= currentPage + 5; i++) {
                 pages.push(i)
-                if (i == totalPages) break
+                if (i === totalPages) break
             }
         } else {
             for (let i = 1; i <= 10; i++) {
                 pages.push(i)
-                if (i == totalPages) break
+                if (i === totalPages) break
             }
         }
     } else {
@@ -31,30 +31,13 @@ export const PaginatedPage = ({totalCards, countPages, onPageChanged, currentPag
 
     return (
         <div className="pagination">
-            <div className="dashboard__indent">
+            {pages.length > 1 && <div className="dashboard__indent">
                 <div className="pagination__body">
                     {pages.map((p, key) => <span key={key} onClick={() => onPageChanged(p)}
                                                  className={`pagination__item${currentPage === p ? ' active' : ''}`}>
                             {p}</span>)}
                 </div>
-            </div>
+            </div>}
         </div>
-        // <div className="pagination">
-        //     <div className="dashboard__indent">
-        //         <div className="pagination__body">
-        //             <button className="pagination__nav" onClick={clickHandlerMin} disabled={startPages <= 1}>⟪⟪⟪
-        //             </button>
-        //             {pages.map((p, key) =>
-        //                 <button key={key}
-        //                         className={`pagination__item${currentPage === p ? ' active' : ''}`}
-        //                         onClick={() => onPageChanged(p)}> {p}
-        //                 </button>
-        //             )}
-        //             <button className="pagination__nav" onClick={clickHandlerPlus} disabled={endPages > totalPages}>⟫⟫⟫
-        //             </button>
-        //         </div>
-        //     </div>
-        // </div>
-        // );
     )
 };
