a2e5168b1b35ff9b0ff9b4fff0e7e071f08a3414
vasyok28 <waitoatime@gmail.com>
2022-07-17 17:12:12 +0300

    refactoring pack init state

--- src/pages/pack/s2-bll/PackInitState.ts
+++ src/pages/pack/s2-bll/PackInitState.ts
@@ -1,32 +1,30 @@
 import {CardPacksType, GetPackRequestType} from "../s3-dal/PackApi";
 
 export type PackInitStateType = GetPackRequestType & {
-    isInit: 'idle' | 'pre' | 'init'
+    cardPacks: CardPacksType[],
+    cardPacksTotalCount: number
+    minCardsCount: number
+    maxCardsCount: number
+    page: number
+    pageCount: number
     search?: string | null
     packName?: string
     sortPacks?: string
     minCards?: number,
     maxCards?: number,
-    cardPacks: CardPacksType[],
-    cardPacksTotalCount: number
-    maxCardsCount: number
-    minCardsCount: number
-    page: number
-    pageCount: number
 }
 
 export const PackInitState: PackInitStateType = {
+    user_id: undefined,
     cardPacks: [],
     cardPacksTotalCount: 10,
     minCardsCount: 0,
     maxCardsCount: 500,
     page: 1,
     pageCount: 6,
-    isInit: 'idle',
     search: null,
     packName: 'updated',
     sortPacks: 'updated',
     minCards: 0,
     maxCards: 1,
-    user_id: undefined,
 }
